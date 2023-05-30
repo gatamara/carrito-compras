32603c80bf70b13c7e7f80e74c8507767ddf28e2
Tamara Salinas <gatamara29@hmail.com>
2023-05-29 23:16:14 -0400

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import {products as initialProducts} from './mocks/products.json'
+import './App.css'
+import { useState } from 'react'
+import { Header } from './components/Header'
+import { Products } from './components/Products'
+import { Footer } from './components/Footer'
+import {useFilters} from './hooks/useFilters'
+import { Cart } from './components/Cart'
+import {CartProvider} from './context/cart'
+
+
+
+
+function App() {
+
+  const [products]=useState(initialProducts)
+
+  const { filterProducts}=useFilters()
+
+  const filteredProducts= filterProducts(initialProducts)
+
+  return (
+    <CartProvider>
+    <Header  />
+    <Cart/>
+    <Products products={filteredProducts} />
+    <Footer/>
+    </CartProvider>
+    
+  )
+}
+
+export default App
